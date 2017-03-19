80bbfeaab1fa04a550ead1c5c836a56d3ac1f900
Dustin Steadman (dukesmart) <dukesmart@users.noreply.github.com>
2017-03-19 07:15:09 -0700

    Add files via upload

---
+++ Recall/index.php
@@ -0,0 +1,5 @@
+<?php
+@include 'template.php';
+echo $template_header;
+echo $template_end;
+?>
---
+++ Recall/template.php
@@ -0,0 +1,13 @@
+<?php
+$template_header = '
+<html>
+<head>
+		<meta name="author" content="Dustin Steadman, Isabelle Larson, Weylin MacCalla, Courtney Thurston, David Krumrey, David Coffield" />
+		<title>Recall</title>
+		<link rel="stylesheet" href="styles.css" />
+</head>
+<body>';
+		
+$template_end ='</body>
+</html>';
+?>

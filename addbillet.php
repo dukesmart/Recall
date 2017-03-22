b4fa78dfb474019fc1f2b5a89e3e5e32083a528b
Dustin Steadman <STEADMAD@my.erau.edu>
2017-03-22 09:47:07 -0400

    Fixed ading billets

--- addbillet.php
+++ addbillet.php
@@ -12,37 +12,49 @@ check_post();
  * Check POST variables to see if are contents to submit.
  */
 function check_post() {
-	
-}
-if(!isset($_POST['billetname']) || ($_POST['billetname'] == "") || !isset($_POST['department']) || ($_POST['department'] == "")) {
-	echo $template_footer;
-	exit();
-} else {
-	submit();
+	global $_POST;
+	if(!isset($_POST['billetname']) || ($_POST['billetname'] == "")/* || !isset($_POST['department']) || ($_POST['department'] == "")*/) {
+		display_unsubmitted_page_contents();
+		echo $template_footer;
+		exit();
+	} else {
+		submit();
+	}
 }
 
 /**
  * Sets variables, connects to database, and inserts contents into database.
  */
 function submit() {
+	global $mysql_connection, $billet_name, $template_footer, $mysql_host, $mysql_username, $mysql_password, $mysql_database, $query_result;
 	check_vars();
+	
 	/* Connect to the MySQL server */
-	$mysql_connection = connect();
-	if(!$mysql_connection){
+	$mysql_connection = mysqli_connect($mysql_host, $mysql_username, $mysql_password, $mysql_database);
+	if (!$mysql_connection) {
+		/* Couldn't connect */
+		echo '<p class="error0">Error: Unable to connect to MySQL.</p>\n';
+		echo $template_footer;
 		exit();
+	} else {
+		/* Connected */
+		$query_result = mysqli_query($mysql_connection, "INSERT INTO billets (name) VALUES ('" . $billet_name . "');");
+		if($query_result) {
+			echo '<p>Success: ' . $billet_name . " added.</p>";
+		} else {
+			echo '<p>Success! </p><p class="error1">Error: Could not add department to database.</p>';
+		}
+		display_submitted_page_contents();
+	
+		mysqli_close();
 	}
-	
-	/* Connected */
-	$query_result = mysqli_query($mysql_connection, "INSERT INTO billets (name) VALUES (`" . $billet_name . "`);");
-	display_submitted_page_contents();
-	
-	mysqli_close();
 }
 
 /**
  * Filter submitted contents and set the variables locally.
  */
 function check_vars() {
+	global $billet_name;
 	$billet_name = filter_var($_POST['billetname'], FILTER_SANITIZE_STRING);
 }
 
@@ -50,7 +62,11 @@ function check_vars() {
  * Display the contents of the page after the form has been submitted.
  */
 function display_submitted_page_contents() {
-	
+	echo '<table class="left">
+			<tr><td><a href="addbillet.php">Add another billet</a></td></tr>
+			<tr><td><a href="index.php">Return</a></td></tr>
+		</table>';
+	echo $template_footer;
 }
 
 /**

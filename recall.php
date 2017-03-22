a078dcdb17a817026a0e5f679d79c9d30c18dd33
Dustin Steadman <STEADMAD@my.erau.edu>
2017-03-22 09:54:26 -0400

    Formatting

--- recall.php
+++ recall.php
@@ -7,15 +7,18 @@
 @include 'template.php';
 
 echo $template_header;
-echo '<p><a href="index.php">Return</a></p>';
-echo $template_footer;
 check_post();
 
 /**
  *  Check POST variables to see if are contents to submit.
  */
 function check_post() {
-	
+	// TODO Fix
+	if(true) {
+		display_unsubmitted_page_contents();
+	} else {
+		submit();
+	}
 }
 
 /**
@@ -46,13 +49,18 @@ function check_vars() {
  * Display the contents of the page after the form has been submitted.
  */
 function display_submitted_page_contents() {
-
+	echo '<p><a href="index.php">Return</a></p>';
+	echo $template_footer;
 }
 
 /**
  * Display the submission form page contents.
  */
 function display_unsubmitted_page_contents() {
-	
+	echo '<form name="recall" action="" method="POST">
+			<input type="submit" value="Start a Recall" />
+			</form>';
+	echo '<p><a href="index.php">Return</a></p>';
+	echo $template_footer;
 }
 ?>

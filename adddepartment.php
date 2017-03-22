e60e77196dc0ab31067e75fe89c1d2a9a51b2d84
Dustin Steadman <STEADMAD@my.erau.edu>
2017-03-21 20:06:16 -0400

    Adding departments to database

--- adddepartment.php
+++ adddepartment.php
@@ -3,7 +3,7 @@
 @include config.php;
 
 echo $template_header;
-if(!isset($_POST['departmentname']) || ($_POST['departmentname'] == null)) {
+if(!isset($_POST['departmentname']) || !isset($_POST['rootbillet']) || ($_POST['departmentname'] == "") || ($_POST['rootbillet'] == "")) {
 	echo '<form name="adddepartmentform" action="adddepartment.php" method="POST">
 		<table class="center">
 				<tr>
@@ -15,7 +15,11 @@ if(!isset($_POST['departmentname']) || ($_POST['departmentname'] == null)) {
 				</tr>
 				<tr>
 				<td>Root Billet:</td>
-				<td></td>
+				<td>
+					<select name="department">
+						' . /* TODO: Dynamically generate billet list*/ '
+					</select>
+				</td>
 				</tr>
 				<tr>
 				<td></td>
@@ -25,5 +29,21 @@ if(!isset($_POST['departmentname']) || ($_POST['departmentname'] == null)) {
 </form>';
 	echo $template_footer;
 	exit();
+} else {
+	$department_name = filter_var($_POST['departmentname'], FILTER_SANITIZE_STRING);
+	//$department_root_billet = filter_var($_POST['rootbillet'], FILTER_SANITIZE_NUMBER_INT);
+}
+
+/* Connect to the MySQL server */
+$mysql_connection = mysqli_connect($mysql_host, $mysql_username, $mysql_password, $mysql_database);
+if (!$mysql_connection) {
+	/* Couldn't connect */
+	echo '<p class="error0">Error: Unable to connect to MySQL.</p>\n';
+	echo $template_footer;
+	exit;
+} else {
+	/* Connected */
+	$query_result = mysqli_query($mysql_connection, "INSERT INTO departments (name) VALUES (`" . $department_name . "`);");
+
 }
 ?>

08bdbdc265afaeb274796654c8c0ad6f68060ab0
Dustin Steadman <STEADMAD@my.erau.edu>
2017-03-28 18:03:58 -0400

    Added jQuery reference

--- template.php
+++ template.php
@@ -9,6 +9,7 @@
 	<title>Recall</title>
 	<meta name="author" content="Dustin Steadman, Isabelle Larson, Weylin MacCalla, Courtney Thurston, David Krumrey, David Coffield" />
 	<link rel="stylesheet" href="styles.css" />
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js" /></script>
 </head>
 <body>';
 

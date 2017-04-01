85189b1646fb3d269c20078d59e127f557ea1964
Dustin Steadman <STEADMAD@my.erau.edu>
2017-03-31 20:45:49 -0400

    Started settings page

---
+++ settings.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * This page is the page in which an administrator may edit the administrative settings.
+ * @package recall.php
+ */
+
+@include 'config.php';
+@include 'template.php';
+
+/**
+ * Check to see if a user has previously logged in.
+ */
+function check_session() {
+	global $_SESSION;
+	global $template_header;
+	
+	if(isset($_SESSION['email'])) {
+		echo $template_header;
+		check_post();
+	} else {
+		header("location:index.php");
+	}
+}
+
+/**
+ *  Check POST variables to see if are contents to submit.
+ */
+function check_post() {
+	if(!isset($_GET['searchquery'])) {
+		display_unsubmitted_page_contents();
+		exit();
+	} else {
+		submit();
+	}
+}
+
+/**
+ * Sets variables, connects to database, submits query to database.
+ */
+function submit() {
+	global $mysql_error_connect, $template_footer;
+	global $mysql_connection, $mysql_host, $mysql_username, $mysql_password, $mysql_database;
+	check_vars();
+	
+	/* Connect to the MySQL server */
+	$mysql_connection = mysqli_connect($mysql_host, $mysql_username, $mysql_password, $mysql_database);
+	if (!$mysql_connection) {
+		/* Couldn't connect */
+		echo $mysql_error_connect;
+		echo $template_footer;
+		exit;
+	} else {
+		/* Connected */
+		// TODO Search
+		display_submitted_page_contents();
+		
+		mysqli_close();
+	}
+}
+
+/**
+ * Filter submitted contents and set the variables locally.
+ */
+function check_vars() {
+	$sanitized_searchquery = filter_var($_GET['searchquery'], FILTER_SANITIZE_STRING);
+}
+
+/**
+ * Display the contents of the page after the form has been submitted.
+ */
+function display_submitted_page_contents() {
+	global $template_footer, $nav_sidebar, $template_footer;
+	
+	echo $nav_sidebar;
+	
+	echo $template_footer;
+}
+
+/**
+ * Display the submission form page contents.
+ */
+function display_unsubmitted_page_contents() {
+	global $template_footer, $nav_sidebar, $template_footer;
+	
+	echo $template_header;
+	echo $nav_sidebar;
+	echo '<p>You must be participating in a recall to submit a confirmation.</p>';
+	echo $template_footer;
+}
+
+?>

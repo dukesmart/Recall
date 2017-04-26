4a67f0a4985f4775b8aeb3216ec335163f5cfee5
dcoffield97 <coffield@my.erau.edu>
2017-04-26 09:39:42 -0400

    Update search.php

--- search.php
+++ search.php
@@ -46,7 +46,7 @@ function search_results(){
 				//connect  to the database
 				$db=mysql_connect  ("servername", "username",  "password") or die ('I cannot connect to the database  because: ' . mysql_error());
 				//-select  the database to use
-				$mydb=mysql_select_db("yourDatabase");
+				$mydb=mysql_select_db("recall");
 				//-query  the database table
 				$sql="SELECT firstname, lastname FROM users WHERE lastname LIKE '%" . $name .  "%'";
 				//-run  the query against the mysql query function

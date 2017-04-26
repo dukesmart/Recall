18bef0a0be847dcb1a9bd089184182df37e99b31
Dustin Steadman <STEADMAD@my.erau.edu>
2017-04-25 20:29:09 -0400

    Added confirming via emailed link

--- confirm.php
+++ confirm.php
@@ -6,7 +6,11 @@
 @include 'template.php';
 @include 'lib.php';
 
-check_post();
+if(mysql_setup()) {
+	check_post();
+} else {
+	exit();
+}
 
 /**
  *  Check POST variables to see if are contents to submit.
@@ -24,22 +28,53 @@ function check_post() {
  * Sets variables, connects to database, submits query to database.
  */
 function submit() {
-	global $mysql_error_connect, $template_footer;
-	global $mysql_connection;
+	global $template_header, $template_footer;
+	global $mysql_connection, $sanitized_id;
 	check_vars();
 	
-	/* TODO Get userid, recallid based on sanitized_id */
-	//$query_result = mysqli_query($mysql_connection, "SELECT * FROM recipients WHERE recipientid")
+	echo $template_header;
+	$confirmation_datetime = date("Y-m-d H:i:s");
 
-	/* TODO Insert confirmation into database */
-	//$query_result = mysqli_query($mysql_connection, "INSERT INTO confirmations");
-	display_submitted_page_contents();
+	$recipient_query = mysqli_query($mysql_connection, "SELECT * FROM recipients WHERE recipientid='" . $sanitized_id. "';");
+	if(!$recipient_query) {
+		// Query failed
+		echo '<div class="alert alert-danger" role="alert">Error: Failed to decect recipient ' . $sanitized_id . '. </div></main>' . PHP_EOL;
+		echo $template_footer;
+		exit();
+	} else {
+		$recipient = $recipient_query->fetch_assoc();
+		$status = '1';
+		$verification_query = mysqli_query($mysql_connection, "SELECT * FROM confirmations WHERE userid='" . $recipient['userid'] . "' AND recallid='" . $recipient['recallid']. "';");
+		if(!$verification_query) {
+			// Query failed
+			echo '<div class="alert alert-danger" role="alert">Error: Failed to check existing confirmations. </div></main>' . PHP_EOL;
+			echo $template_footer;
+			exit();
+		} else {
+			if($verification_query->num_rows != 0) {
+				echo '<div class="alert alert-danger" role="alert">Error: Already confirmed. </div></main>' . PHP_EOL;
+				echo $template_footer;
+				exit();
+			} else {
+				$confirmation_query = mysqli_query($mysql_connection, "INSERT INTO confirmations (userid, recallid, datetime, status) VALUES ('" . $recipient['userid'] . "', '" . $recipient['recallid']. "', '" . $confirmation_datetime . "', '" . $status . "');");
+				if(!$confirmation_query) {
+					// Query failed
+					echo '<div class="alert alert-danger" role="alert">Error: Failed to submit confirmation. </div></main>' . PHP_EOL;
+					echo $template_footer;
+					exit();
+				} else {
+					display_submitted_page_contents();
+				}
+			}
+		}
+	}
 }
 
 /**
  * Filter submitted contents and set the variables locally.
  */
 function check_vars() {
+	global $sanitized_id;
 	$sanitized_id = filter_var($_GET['id'], FILTER_SANITIZE_STRING);
 }
 
@@ -47,8 +82,9 @@ function check_vars() {
  * Display the contents of the page after the form has been submitted.
  */
 function display_submitted_page_contents() {
-	global $template_footer;
-	
+	global $template_footer, $template_footer;
+	echo '<div class="alert alert-success" role="alert">Confirmation submitted.</div>' . PHP_EOL;
+	echo '</main>' . PHP_EOL;
 	echo $template_footer;
 }
 
@@ -56,6 +92,7 @@ function display_submitted_page_contents() {
  * Display the submission form page contents.
  */
 function display_unsubmitted_page_contents() {
+	global $template_header, $template_footer;
 	echo $template_header;
 	echo '<p>You must be participating in a recall to submit a confirmation.</p>';
 	echo $template_footer;
